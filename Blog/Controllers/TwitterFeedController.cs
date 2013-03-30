f4957c21060376929980e467cbfcb1d214c2f6a8
Paul David Welbourne <paulwelbourne@yahoo.co.uk>
2013-03-30 18:26:50 +0000

    Modified TwitterFeed Async code to Take() before the Select() and subsequent ToList().

--- Blog/Controllers/TwitterFeedController.cs
+++ Blog/Controllers/TwitterFeedController.cs
@@ -16,18 +16,15 @@ namespace Blog.Controllers
 
 		public void IndexAsync()
 		{
+			// TODO : Make this value configurable.
 			const string userName = "wellers";
 
 			AsyncManager.OutstandingOperations.Increment();
-			// TODO : Make this value configurable.
 			GetTwitterFeed(userName);
 		}
 
 		public ActionResult IndexCompleted(List<TwitterFeedItem> tweets)
 		{
-			// TODO : Make this value configurable.
-			const int numberOfTweets = 5;
-
 			TwitterFeedViewModel model;
 			if (tweets == null || !tweets.Any())
 			{
@@ -35,54 +32,57 @@ namespace Blog.Controllers
 			}
 			else
 			{
-				model = new TwitterFeedViewModel { Tweets = tweets.Take(numberOfTweets) };
+				model = new TwitterFeedViewModel { Tweets = tweets };
 			}
 
 			return PartialView("TwitterFeed", model);
 		}
 
-        private void GetTwitterFeed(string screenName)
-        {
-            if (string.IsNullOrEmpty(screenName))
-                throw new ArgumentNullException("screenName");
+		private void GetTwitterFeed(string screenName)
+		{
+			if (string.IsNullOrEmpty(screenName))
+				throw new ArgumentNullException("screenName");
 
-            // check the cache for Tweets
-            List<TwitterFeedItem> items;
-            if (CacheHelper.TryGet<List<TwitterFeedItem>>(_cacheKey, out items))
-            {
-                AsyncManager.Parameters["tweets"] = items.ToList();
-                AsyncManager.OutstandingOperations.Decrement();
-                return;
-            }
+			// check the cache for Tweets
+			List<TwitterFeedItem> items;
+			if (CacheHelper.TryGet<List<TwitterFeedItem>>(_cacheKey, out items))
+			{
+				AsyncManager.Parameters["tweets"] = items.ToList();
+				AsyncManager.OutstandingOperations.Decrement();
+				return;
+			}
 
-            var twitter = new WebClient();
+			var twitter = new WebClient();
 
-            twitter.DownloadStringCompleted += (sender, e) =>
-            {
-                if (e.Error != null)
-                    return;
+			twitter.DownloadStringCompleted += (sender, e) =>
+			{
+				// TODO : Make this value configurable.
+				const int numberOfTweets = 5;
 
-                XElement xmlTweets = XElement.Parse(e.Result);
-                var tweets = xmlTweets.Descendants("status").Select(tweet => new TwitterFeedItem
-                {
-                    ImageSource = tweet.Element("user").Element("profile_image_url").Value,
-                    Message = tweet.Element("text").Value,
-                    UserName = tweet.Element("user").Element("screen_name").Value,
-                    PostedDate = tweet.Element("created_at").Value.ParseTwitterDateTime()
-                });
+				if (e.Error != null)
+					return;
 
-                // add tweets to cache
-                var tweetsList = tweets.ToList();
+				XElement xmlTweets = XElement.Parse(e.Result);
+				var tweets = xmlTweets.Descendants("status").Take(numberOfTweets).Select(tweet => new TwitterFeedItem
+				{
+					ImageSource = tweet.Element("user").Element("profile_image_url").Value,
+					Message = tweet.Element("text").Value,
+					UserName = tweet.Element("user").Element("screen_name").Value,
+					PostedDate = tweet.Element("created_at").Value.ParseTwitterDateTime()
+				});
 
-                // TODO : make timeout configurable.
-                CacheHelper.Add<List<TwitterFeedItem>>(tweetsList, _cacheKey, (double)30);
+				// add tweets to cache
+				var tweetsList = tweets.ToList();
 
-                AsyncManager.Parameters["tweets"] = tweetsList;
-                AsyncManager.OutstandingOperations.Decrement();
-            };
+				// TODO : make timeout configurable.
+				CacheHelper.Add<List<TwitterFeedItem>>(tweetsList, _cacheKey, (double)30);
 
-            var feedUrl = new Uri(string.Format("http://api.twitter.com/1/statuses/user_timeline.xml?screen_name={0}", screenName));
-            twitter.DownloadStringAsync(feedUrl);
-        }
+				AsyncManager.Parameters["tweets"] = tweetsList;
+				AsyncManager.OutstandingOperations.Decrement();
+			};
+
+			var feedUrl = new Uri(string.Format("http://api.twitter.com/1/statuses/user_timeline.xml?screen_name={0}", screenName));
+			twitter.DownloadStringAsync(feedUrl);
+		}
 	}
 }
