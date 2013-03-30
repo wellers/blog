05f8ba05a5bbbdea845d8c96f2bc1223c59567df
Paul David Welbourne <paulwelbourne@yahoo.co.uk>
2013-03-30 12:36:12 +0000

    Added Core project with CacheHelper. Modified FirstBlogEntry migration.

---
+++ Blog.Core/Utility/CacheHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Web;
+
+namespace Blog.Core.Utility
+{
+    public static class CacheHelper
+    {
+        /// <summary>
+        /// Insert value into the cache using
+        /// appropriate name/value pairs
+        /// </summary>
+        /// <typeparam name="T">Type of cached item</typeparam>
+        /// <param name="o">Item to be cached</param>
+        /// <param name="key">Name of item</param>
+        /// <param name="timeout">Timeout of cached item in minutes</param>
+        public static void Add<T>(T o, string key, double timeout) where T : class
+        {
+            // NOTE: Apply expiration parameters as you see fit.
+            // In this example, I want an absolute
+            // timeout so changes will always be reflected
+            // at that time. Hence, the NoSlidingExpiration.
+            HttpContext.Current.Cache.Insert(
+                key,
+                o,
+                null,
+                DateTime.Now.AddMinutes(timeout),
+                System.Web.Caching.Cache.NoSlidingExpiration);
+        }
+
+        /// <summary>
+        /// Remove item from cache
+        /// </summary>
+        /// <param name="key">Name of cached item</param>
+        public static void Clear(string key)
+        {
+            HttpContext.Current.Cache.Remove(key);
+        }
+
+        /// <summary>
+        /// Check for item in cache
+        /// </summary>
+        /// <param name="key">Name of cached item</param>
+        /// <returns></returns>
+        public static bool Exists(string key)
+        {
+            return HttpContext.Current.Cache[key] != null;
+        }
+
+        /// <summary>
+        /// Retrieve cached item if 
+        /// </summary>
+        /// <typeparam name="T">Type of cached item</typeparam>
+        /// <param name="key">Name of cached item</param>
+        /// <returns>Cached item as type</returns>
+        public static bool TryGet<T>(string key, out T cachedItem) where T : class
+        {
+            try
+            {
+                if (Exists(key))
+                {
+                    cachedItem = (T)HttpContext.Current.Cache[key];
+                    return true;
+                }
+                cachedItem = null;
+                return false;
+            }
+            catch
+            {
+                cachedItem = null;
+                return false;
+            }
+        }
+    }
+}
--- Blog.Data.CodeFirst/Migrations/201303282022132_FirstBlogEntry.cs
+++ Blog.Data.CodeFirst/Migrations/201303282022132_FirstBlogEntry.cs
@@ -10,16 +10,34 @@ namespace Blog.Data.CodeFirst.Migrations
             Sql(@"
                 INSERT INTO BlogEntries (Title, Entry, PostedDate)
                 SELECT 'Syntactic sugar: one lump or two?',
-                '<div>
-                    <p>This is my first blog post. Hooray! So I thought, for the contents of my first post, I would write about some of the features I like about the Dot Net framework, in particular C# syntactic sugar.</p>
-                    <p>Quite simply, syntactic sugar is syntax within a programming language that is designed to make things easier to read or to express.</p>
-                    <p>Some examples of syntactic sugar are detailed below.</p>
-                </div>
-                <div>
-                    <br />
-                    <p><strong>LINQ (Language Integrated Query)</strong></p>
-                    <p>LINQ statements are extensions to Dot Net that add query-like capabilities. This can be extremely powerful when using in conjunction with collection. An example of this is shown below.</p>
-<pre class=""brush: csharp"">
+                '<div><p>This is my first blog post. Hooray! So I thought, for the contents of my first post, I would write about some of the features I like about the Dot Net framework, in particular C# syntactic sugar.</p><p>Quite simply, syntactic sugar is syntax within a programming language that is designed to make things easier to read or to express.</p><p>Some examples of syntactic sugar are detailed below.</p></div><div><br /><p><strong>Auto-properties</strong></p><p>Automatic properties are great. Removing the need to add a backing field to hold data.</p><pre class=""brush: csharp"">
+// property with a backing field
+private int _id;
+public int ID
+{
+	get 
+	{ 
+		return _id; 
+	}
+	set 
+	{
+		_id = value; 
+	}
+}
+
+// this can be simple rewritten to
+public int ID
+{
+	get;
+	set;
+}
+</pre><p>What is really great is when you only want the property''s ''getter'' to be public. You can write your property as follows.</p><pre class=""brush: csharp"">
+public int ID
+{
+	get;
+	private set;
+}
+</pre><p>This property is now immutable and therefore can only be set from within a class that contains it.</p></div><div><br /><p><strong>LINQ (Language Integrated Query)</strong></p><p>LINQ statements are extensions to Dot Net that add query-like capabilities. This can be extremely powerful when used in conjunction with collection. An example of this is shown below.</p><pre class=""brush: csharp"">
     var items = new List&ltItem&gt();
 
     // foreach for projecting the property ID into a new collection
@@ -30,13 +48,7 @@ namespace Blog.Data.CodeFirst.Migrations
     }
 
     // this can be easily rewritten as...
-    var ids = items.Select(x =&gt new { x.ID });</pre>
-                </div>
-                <div>
-                    <br />
-                    <p><strong>Object and Collection initialisers</strong></p>
-                    <p>Object initialisers are a great way of instaniating objects, with members set, without the tediousness of writing out member assignments. An example is shown below.</p>
-<pre class=""brush: csharp"">
+    var ids = items.Select(x =&gt new { x.ID });</pre></div><div><br /><p><strong>Object and Collection initialisers</strong></p><p>Object initialisers are a great way of instantiating objects, with their members set, without the tediousness of writing member assignments. An example is shown below.</p><pre class=""brush: csharp"">
     // with the following class
     public class Person
     {
@@ -50,9 +62,7 @@ namespace Blog.Data.CodeFirst.Migrations
     person.Name = ""Paul"";
 
     // or we can do this
-    var person = new Person { ID = 1, Name = ""Paul"" };</pre>
-                    <p>Collections can also be initialised, with their members set, using a similar syntax.</p>
-<pre class=""brush: csharp"">
+    var person = new Person { ID = 1, Name = ""Paul"" };</pre><p>Collections can also be initialised, with their members set, using a similar syntax.</p><pre class=""brush: csharp"">
     // initialise collection and add members
     var dictionary = new Dictionary&ltint, string&gt();
     dictionary.Add(1,""Foo"");
@@ -63,11 +73,7 @@ namespace Blog.Data.CodeFirst.Migrations
     {
         { 1, ""Foo"" }, 
         { 2, ""Bar"" } 
-    };</pre>
-                </div>
-                <div>
-                    <p>For my next post I may continue to expand on this post and go into other syntactic sugar features of Dot Net.</p>
-                </div>',
+    };</pre></div><div><p>I may continue to expand on this post, for my next post, and go into other syntactic sugar features of Dot Net.</p></div>',
                 GETDATE()
             ");
         }
