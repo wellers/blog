4859bf402b4dd640e0467ee9e483b8a6523c6886
wellers <paulwelbourne@gmail.com>
2024-07-04 20:33:47 +0100

    Tidy-up Global.asax.cs.

--- Blog/Global.asax.cs
+++ Blog/Global.asax.cs
@@ -3,14 +3,10 @@ using System.Web.Routing;
 
 namespace Blog
 {
-	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-	// visit http://go.microsoft.com/?LinkId=9394801
-
 	public class MvcApplication : System.Web.HttpApplication
 	{
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
 		{
-			//filters.Add(new HandleErrorAttribute());
 		}
 
 		public static void RegisterRoutes(RouteCollection routes)
